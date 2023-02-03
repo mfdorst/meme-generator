90066c370bb7fa652ae259f4552fa3ea08ec3740
Michael Dorst <m@mdorst.net>
2023-02-02 22:54:57 -0800

    Use single state variable for meme

--- src/Meme.js
+++ src/Meme.js
@@ -3,23 +3,25 @@ import memesData from './memesData.js'
 
 export default function Meme() {
 
-  const [memeUrl, setMemeUrl] = useState()
-  const [memeName, setMemeName] = useState()
-
   function getRandomMeme() {
     const memes = memesData.data.memes
     const randomIndex = Math.floor(Math.random() * memes.length)
     const {url, name} = memes[randomIndex]
-    setMemeUrl(url)
-    setMemeName(name)
+    return {url, name}
+  }
+
+  const [meme, setMeme] = useState(getRandomMeme())
+
+  function updateMeme() {
+    setMeme(getRandomMeme())
   }
 
   return (
     <main>
       <input className='input-top' placeholder='Shut up' />
       <input className='input-botom' placeholder='and take my money' />
-      <button className='purple-gradient' onClick={() => console.log(getRandomMeme())}>Get a new meme image</button>
-      <img className='meme' src={memeUrl} alt={memeName} />
+      <button className='purple-gradient' onClick={updateMeme}>Get a new meme image</button>
+      <img className='meme' src={meme.url} alt={meme.name} />
     </main>
   )
 }
