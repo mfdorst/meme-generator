88f860e756a4bfe4c4ccc516b81470f67f1fa8da
Michael Dorst <m@mdorst.net>
2023-02-02 22:39:07 -0800

    Render random meme templates on click

--- src/Meme.js
+++ src/Meme.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import memesData from './memesData.js'
 
 export default function Meme() {
+
+  const [memeUrl, setMemeUrl] = useState()
+  const [memeName, setMemeName] = useState()
+
+  function getRandomMeme() {
+    const memes = memesData.data.memes
+    const randomIndex = Math.floor(Math.random() * memes.length)
+    const {url, name} = memes[randomIndex]
+    setMemeUrl(url)
+    setMemeName(name)
+  }
+
   return (
     <main>
-      <form>
-        <input className='input-top' value='Shut up' />
-        <input className='input-botom' value='and take my money' />
-        <button className='purple-gradient'>Get a new meme image</button>
-      </form>
+      <input className='input-top' placeholder='Shut up' />
+      <input className='input-botom' placeholder='and take my money' />
+      <button className='purple-gradient' onClick={() => console.log(getRandomMeme())}>Get a new meme image</button>
+      <img className='meme' src={memeUrl} alt={memeName} />
     </main>
   )
 }
