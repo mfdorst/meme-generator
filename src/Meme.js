1f2a48240c49cd8d9b72b8a626a4f8619841e59c
Michael Dorst <m@mdorst.net>
2023-02-04 17:44:53 -0800

    Add meme text, get memes from imgflip API

--- src/Meme.js
+++ src/Meme.js
@@ -1,27 +1,57 @@
-import React, { useState } from 'react'
-import memesData from './memesData.js'
+import React, { useEffect, useState } from 'react'
 
 export default function Meme() {
+  const [allMemes, setAllMemes] = useState([])
 
-  function getRandomMeme() {
-    const memes = memesData.data.memes
-    const randomIndex = Math.floor(Math.random() * memes.length)
-    const {url, name} = memes[randomIndex]
-    return {url, name}
+  function getRandomMemeImgData() {
+    if (allMemes.length === 0) {
+      return { url: "", name: "" }
+    }
+    const randomIndex = Math.floor(Math.random() * allMemes.length)
+    const { url, name } = allMemes[randomIndex]
+    return { url, name }
   }
 
-  const [meme, setMeme] = useState(getRandomMeme())
+  const [meme, setMeme] = useState({ ...getRandomMemeImgData(), topText: "", bottomText: "" })
 
   function updateMeme() {
-    setMeme(getRandomMeme())
+    setMeme(prevMeme => ({ ...prevMeme, ...getRandomMemeImgData() }))
   }
+
+  function handleChange(e) {
+    setMeme(prevMeme => ({ ...prevMeme, [e.target.name]: e.target.value }))
+  }
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('https://api.imgflip.com/get_memes')
+      const memes = await res.json()
+      setAllMemes(memes.data.memes)
+    })()
+  }, [])
 
   return (
     <main>
-      <input className='input-top' placeholder='Shut up' />
-      <input className='input-botom' placeholder='and take my money' />
+      <input
+        name='topText'
+        className='input-top'
+        placeholder='Shut up'
+        onChange={handleChange}
+        value={meme.topText}
+      />
+      <input
+        name='bottomText'
+        className='input-botom'
+        placeholder='and take my money'
+        onChange={handleChange}
+        value={meme.bottomText}
+      />
       <button className='purple-gradient' onClick={updateMeme}>Get a new meme image</button>
-      <img className='meme' src={meme.url} alt={meme.name} />
+      <div className='meme'>
+        <img className='meme-img' src={meme.url} alt={meme.name} />
+        <h3 className='meme-text top'>{meme.topText}</h3>
+        <h3 className='meme-text bottom'>{meme.bottomText}</h3>
+      </div>
     </main>
   )
 }
